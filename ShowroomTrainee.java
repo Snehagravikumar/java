7aaeed61be4d335602676969ce9f3a08a9469891
Snehagravikumar <snehagravikumar.xworkz@gmail.com>
2023-10-31 19:54:01 +0530

    javacode

---
+++ ShowroomTrainee.java
@@ -0,0 +1,102 @@
+class ShowroomTrainee
+{
+public static void main(String[] args)
+{
+System.out.println("Start main in ShowroomTrainee");
+
+Showroom showroom=new Showroom();
+System.out.println("Showroom name :"+showroom.name);
+System.out.println("Showroom width :"+showroom.width);
+System.out.println("Showroom hieght :"+showroom.height);
+System.out.println("Showroom Product :"+showroom.product);
+System.out.println("Showroom No of Workers :"+showroom.noOfWorkers);
+System.out.println("Showroom No Of Products :"+showroom.noOfProducts);
+System.out.println("Showroom Managere Name :"+showroom.managerName);
+System.out.println("Showroom No Of Branch :"+showroom.noOfBranch);
+
+System.out.println("*******************************");
+
+Showroom showroom1=new Showroom("Hondai");
+System.out.println("Showroom name :"+showroom1.name);
+System.out.println("Showroom width :"+showroom1.width);
+System.out.println("Showroom hieght :"+showroom1.height);
+System.out.println("Showroom Product :"+showroom1.product);
+System.out.println("Showroom No of Workers :"+showroom1.noOfWorkers);
+System.out.println("Showroom No Of Products :"+showroom1.noOfProducts);
+System.out.println("Showroom Managere Name :"+showroom1.managerName);
+System.out.println("Showroom No Of Branch :"+showroom1.noOfBranch);
+
+System.out.println("*******************************");
+
+Showroom showroom2=new Showroom();
+System.out.println("Showroom name :"+showroom2.name);
+System.out.println("Showroom width :"+showroom2.width);
+System.out.println("Showroom hieght :"+showroom2.height);
+System.out.println("Showroom Product :"+showroom2.product);
+System.out.println("Showroom No of Workers :"+showroom2.noOfWorkers);
+System.out.println("Showroom No Of Products :"+showroom2.noOfProducts);
+System.out.println("Showroom Managere Name :"+showroom2.managerName);
+System.out.println("Showroom No Of Branch :"+showroom2.noOfBranch);
+
+System.out.println("*******************************");
+
+Showroom showroom3=new Showroom(550,70);
+System.out.println("Showroom name :"+showroom3.name);
+System.out.println("Showroom width :"+showroom3.width);
+System.out.println("Showroom hieght :"+showroom3.height);
+System.out.println("Showroom Product :"+showroom3.product);
+System.out.println("Showroom No of Workers :"+showroom3.noOfWorkers);
+System.out.println("Showroom No Of Products :"+showroom3.noOfProducts);
+System.out.println("Showroom Managere Name :"+showroom3.managerName);
+System.out.println("Showroom No Of Branch :"+showroom3.noOfBranch);
+
+System.out.println("*******************************");
+
+Showroom showroom4=new Showroom("Car","Sneha");
+System.out.println("Showroom name :"+showroom4.name);
+System.out.println("Showroom width :"+showroom4.width);
+System.out.println("Showroom hieght :"+showroom4.height);
+System.out.println("Showroom Product :"+showroom4.product);
+System.out.println("Showroom No of Workers :"+showroom4.noOfWorkers);
+System.out.println("Showroom No Of Products :"+showroom4.noOfProducts);
+System.out.println("Showroom Managere Name :"+showroom4.managerName);
+System.out.println("Showroom No Of Branch :"+showroom4.noOfBranch);
+
+System.out.println("*******************************");
+
+Showroom showroom5=new Showroom(3000,"Kia");
+System.out.println("Showroom name :"+showroom5.name);
+System.out.println("Showroom width :"+showroom5.width);
+System.out.println("Showroom hieght :"+showroom5.height);
+System.out.println("Showroom Product :"+showroom5.product);
+System.out.println("Showroom No of Workers :"+showroom5.noOfWorkers);
+System.out.println("Showroom No Of Products :"+showroom5.noOfProducts);
+System.out.println("Showroom Managere Name :"+showroom5.managerName);
+System.out.println("Showroom No Of Branch :"+showroom5.noOfBranch);
+
+System.out.println("*******************************");
+
+Showroom showroom6=new Showroom("car",500);
+System.out.println("Showroom name :"+showroom6.name);
+System.out.println("Showroom width :"+showroom6.width);
+System.out.println("Showroom hieght :"+showroom6.height);
+System.out.println("Showroom Product :"+showroom6.product);
+System.out.println("Showroom No of Workers :"+showroom6.noOfWorkers);
+System.out.println("Showroom No Of Products :"+showroom6.noOfProducts);
+System.out.println("Showroom Managere Name :"+showroom6.managerName);
+System.out.println("Showroom No Of Branch :"+showroom6.noOfBranch);
+
+System.out.println("*******************************");
+
+Showroom showroom7=new Showroom("suzuki",50);
+System.out.println("Showroom name :"+showroom7.name);
+System.out.println("Showroom width :"+showroom7.width);
+System.out.println("Showroom hieght :"+showroom7.height);
+System.out.println("Showroom Product :"+showroom7.product);
+System.out.println("Showroom No of Workers :"+showroom7.noOfWorkers);
+System.out.println("Showroom No Of Products :"+showroom7.noOfProducts);
+System.out.println("Showroom Managere Name :"+showroom7.managerName);
+System.out.println("Showroom No Of Branch :"+showroom7.noOfBranch);
+
+}
+}

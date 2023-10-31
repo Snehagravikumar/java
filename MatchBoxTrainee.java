4973aab72c4afad2f67e56a1575eea868ef562bb
Snehagravikumar <snehagravikumar.xworkz@gmail.com>
2023-10-31 15:24:54 +0530

    Match

---
+++ MatchBoxTrainee.java
@@ -0,0 +1,98 @@
+class MatchBoxTrainee
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Start main in MatchBoxTrainee");
+		
+		
+		MatchBox matchbox=new MatchBox();
+		System.out.println(" MatchBox Brand :"+matchbox.brand);
+		System.out.println(" MatchBox No of Sticks :"+matchbox.noOfSticks);
+		System.out.println(" MatchBox Length :"+matchbox.length);
+		System.out.println(" MatchBox Cost in Rs :"+matchbox.costInRupees);
+		System.out.println(" MatchBox Size :"+matchbox.size);
+        System.out.println(" MatchBox Wieght in Gram :"+matchbox.wieghtInGram);
+		System.out.println(" MatchBox Type :"+matchbox.type);
+
+		System.out.println("*******************************************");
+
+		
+		MatchBox matchbox1=new MatchBox("Tiger");
+		System.out.println(" MatchBox Brand :"+matchbox1.brand);
+		System.out.println(" MatchBox No of Sticks :"+matchbox1.noOfSticks);
+		System.out.println(" MatchBox Length :"+matchbox1.length);
+		System.out.println(" MatchBox Cost in Rs:"+matchbox1.costInRupees);
+		System.out.println(" MatchBox Size :"+matchbox1.size);
+        System.out.println(" MatchBox Wieght in Gram :"+matchbox1.wieghtInGram);
+		System.out.println(" MatchBox Type :"+matchbox1.type);
+		
+		
+		System.out.println("*******************************************");
+
+		
+		MatchBox matchbox2=new MatchBox();
+		System.out.println(" MatchBox Brand :"+matchbox2.brand);
+		System.out.println(" MatchBox No of Sticks :"+matchbox2.noOfSticks);
+		System.out.println(" MatchBox Length :"+matchbox2.length);
+		System.out.println(" MatchBox Cost in Rs :"+matchbox2.costInRupees);
+		System.out.println(" MatchBox Size :"+matchbox2.size);
+        System.out.println(" MatchBox Wieght in Gram :"+matchbox2.wieghtInGram);
+		System.out.println(" MatchBox Type :"+matchbox2.type);
+		
+		
+		System.out.println("*******************************************");
+
+		
+		MatchBox matchbox3=new MatchBox(12);
+		System.out.println(" MatchBox Brand :"+matchbox3.brand);
+		System.out.println(" MatchBox No of Sticks :"+matchbox3.noOfSticks);
+		System.out.println(" MatchBox Length :"+matchbox3.length);
+		System.out.println(" MatchBox Cost in Rs :"+matchbox3.costInRupees);
+		System.out.println(" MatchBox Size :"+matchbox3.size);
+        System.out.println(" MatchBox Wieght in Gram:"+matchbox3.wieghtInGram);
+		System.out.println(" MatchBox Type :"+matchbox3.type);
+		
+		
+		System.out.println("*******************************************");
+
+		
+		MatchBox matchbox4=new MatchBox(10);
+		System.out.println(" MatchBox Brand :"+matchbox4.brand);
+		System.out.println(" MatchBox No of Sticks :"+matchbox4.noOfSticks);
+		System.out.println(" MatchBox Length :"+matchbox4.length);
+		System.out.println(" MatchBox Cost In Rs:"+matchbox4.costInRupees);
+		System.out.println(" MatchBox Size :"+matchbox4.size);
+        System.out.println(" MatchBox Wieght in Gram :"+matchbox4.wieghtInGram);
+		System.out.println(" MatchBox Type :"+matchbox4.type);
+		
+		System.out.println("*******************************************");
+
+		
+		MatchBox matchbox5=new MatchBox();
+		System.out.println(" MatchBox Brand :"+matchbox5.brand);
+		System.out.println(" MatchBox No of Sticks :"+matchbox5.noOfSticks);
+		System.out.println(" MatchBox Length :"+matchbox5.length);
+		System.out.println(" MatchBox Cost in Rs:"+matchbox5.costInRupees);
+		System.out.println(" MatchBox Size :"+matchbox5.size);
+        System.out.println(" MatchBox Wieght in Gram:"+matchbox5.wieghtInGram);
+		System.out.println(" MatchBox Type :"+matchbox5.type);
+		
+		
+		System.out.println("*******************************************");
+
+		
+		MatchBox matchbox6=new MatchBox("woodenStick",12);
+		System.out.println(" MatchBox Brand :"+matchbox6.brand);
+		System.out.println(" MatchBox No of Sticks :"+matchbox6.noOfSticks);
+		System.out.println(" MatchBox Length :"+matchbox6.length);
+		System.out.println(" MatchBox Cost in Rs:"+matchbox6.costInRupees);
+		System.out.println(" MatchBox Size :"+matchbox6.size);
+        System.out.println(" MatchBox Wieght in Gram:"+matchbox6.wieghtInGram);
+		System.out.println(" MatchBox Type :"+matchbox6.type);
+		
+		
+		
+		System.out.println("End main in MatchBoxTrainee");
+
+	}
+}

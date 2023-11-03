75e63d6237482796796e344965599d7502b85739
Snehagravikumar <149233809+Snehagravikumar@users.noreply.github.com>
2023-11-03 09:47:40 +0530

    technology

---
+++ TechnologiesTrainee.java
@@ -0,0 +1,70 @@
+class TechnologiesTrainee
+{
+ public static void main(String[] args)
+ {
+	 System.out.println("Start main in TechnologiesTrainee");
+	 
+ Technologies technology=new Technologies("XWORKZ",5,"btm",24000,4.5,"java",3300009,"omkar",5,45,2,9.8);
+technology.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology1=new Technologies("tap",7,"btm",30000,4.5,"java",3300009,"vinay",5,45,2,9.8);
+technology1.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology2=new Technologies("kodnest",6,"rajajinagar",32000,4.7,"java,sql",3300009,"sanjay",5,45,2,11.8);
+technology2.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology3=new Technologies("palle",5,"",28000,4.5,"python",4500098,"sunil",7,80,2,15.10);
+technology3.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology4=new Technologies("jspider",7,"rajajinagar",40000,4.5,"java,sql,html",788067,"vinay",5,100,5,30);
+technology4.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology5=new Technologies("qspider",6,"indira nagar",35000,4.0,"automation testing",3300009,"deepak",5,45,4,20);
+technology5.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology6=new Technologies("besant",7,"btm",30000,4.5,"cloud computing",3300009,"mithun",5,120,2,16.5);
+technology6.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology7=new Technologies("tap",7,"malleshwaram",30000,4.5,"vb.net",4500009,"vinay",5,45,3,12.4);
+technology7.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology8=new Technologies("pentagon",8,"yashwanthpur",37000,4.2,"cyber security",110009,"manju",5,80,4,20);
+technology8.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology10=new Technologies("XWORKZ",5,"rajajinagar",24000,4.5,"java",1100009,"omkar",5,45,2,9.8);
+technology10.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology11=new Technologies("vx git",5,"jpnagar",50000,4.5,"ui/ux",550009,"kiran",5,60,2,30);
+technology11.displayTechnologies();
+
+System.out.println("\n");
+
+Technologies technology12=new Technologies("run tech",8,"jaynagar",60000,4.5,"softwaretesting",2500009,"sumith",5,50,2,17);
+technology12.displayTechnologies();
+
+
+	 System.out.println("Start main in TechnologiesTrainee");
+
+}
+
+}

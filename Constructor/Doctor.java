7923c5654df6f91a2f1263fab2464eff4af63281
Snehagravikumar <snehagravikumar.xworkz@gmail.com>
2023-11-05 22:11:32 +0530

    Constructor

---
+++ Constructor/Doctor.java
@@ -0,0 +1,15 @@
+class Doctor
+{
+	String name;
+	int experience;
+	boolean fake;
+	double fees;
+	
+	Doctor(String name,int experience,boolean fake,double fees)
+	{
+		this.name=name;
+		this.experience=experience;
+		this.fake=fake;
+		this.fees=fees;
+	}
+}
